8c97180a4afe986c183139763acdadaddd01f441
nekrassov01 <unclek62@gmail.com>
2024-01-31 22:02:35 +0900

    feat: remove DNS healthcheck

--- lib/constructs/dns.ts
+++ lib/constructs/dns.ts
@@ -14,25 +14,25 @@ export class DNS extends Construct {
   constructor(scope: Construct, id: string, props: DNSProps) {
     super(scope, id);
 
-    // Route53 health check
-    const dnsHealthCheck = new cdk.aws_route53.CfnHealthCheck(this, "HealthCheck", {
-      healthCheckConfig: {
-        type: "HTTP",
-        fullyQualifiedDomainName: props.nlb.loadBalancerDnsName,
-        port: 80,
-        resourcePath: "/",
-        requestInterval: 10,
-        failureThreshold: 3,
-        measureLatency: false,
-      },
-      healthCheckTags: [
-        {
-          key: "Name",
-          value: `${props.serviceName}-${props.area}-healthcheck`,
-        },
-      ],
-    });
-    dnsHealthCheck.node.addDependency(props.nlb);
+    //// Route53 health check
+    //const dnsHealthCheck = new cdk.aws_route53.CfnHealthCheck(this, "HealthCheck", {
+    //  healthCheckConfig: {
+    //    type: "HTTP",
+    //    fullyQualifiedDomainName: props.nlb.loadBalancerDnsName,
+    //    port: 80,
+    //    resourcePath: "/",
+    //    requestInterval: 10,
+    //    failureThreshold: 3,
+    //    measureLatency: false,
+    //  },
+    //  healthCheckTags: [
+    //    {
+    //      key: "Name",
+    //      value: `${props.serviceName}-${props.area}-healthcheck`,
+    //    },
+    //  ],
+    //});
+    //dnsHealthCheck.node.addDependency(props.nlb);
 
     // A record for DNS failover
     const nlbARecord = new cdk.aws_route53.CfnRecordSet(this, "RecordSet", {
@@ -44,10 +44,10 @@ export class DNS extends Construct {
         evaluateTargetHealth: true,
       },
       failover: props.failoverType,
-      healthCheckId: dnsHealthCheck.attrHealthCheckId,
+      //healthCheckId: dnsHealthCheck.attrHealthCheckId,
       hostedZoneId: props.hostedZone.hostedZoneId,
       setIdentifier: `${props.serviceName}-${props.area}-id`,
     });
-    nlbARecord.node.addDependency(dnsHealthCheck);
+    //nlbARecord.node.addDependency(dnsHealthCheck);
   }
 }
